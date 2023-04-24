58e70f49c368ea225a0c29f069fa9f5d7e67c32e
Steven <Briggs.steven369@gmail.com>
2023-04-23 19:20:08 -0700

    Add positive messages when you complete a task

--- components/ChoreList.js
+++ components/ChoreList.js
@@ -1,6 +1,7 @@
 import {
+  Button,
   FlatList,
-  KeyboardAvoidingView,
+  KeyboardAvoidingView, Modal,
   Platform,
   StyleSheet,
   Text,
@@ -8,11 +9,14 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import {useState} from 'react';
+import React, {useState} from 'react';
+
+import {positiveMessages} from '../positiveMessages';
 
 const ChoreList = () => {
   const [lists, setLists] = useState([]);
   const [taskName, setTaskName] = useState('');
+  const [message, setMessage] = useState(null);
 
   const addTask = (newTask) => {
     setLists([...lists, newTask]);
@@ -25,6 +29,18 @@ const ChoreList = () => {
     setLists(updatedLists);
   };
 
+  const handleCheckboxPress = (index) => {
+    setLists((prevState) => {
+      const newList = [...prevState];
+      newList[index].completed = !newList[index].completed;
+      if (newList[index].completed) {
+        const positiveMessage = `Decrypt Says: \"${positiveMessages[Math.floor(Math.random() * positiveMessages.length)]}\"`
+        setMessage(positiveMessage);
+      }
+      return newList;
+    });
+  };
+
   return (
       <View style={styles.container}>
         <FlatList
@@ -32,11 +48,13 @@ const ChoreList = () => {
             data={lists}
             renderItem={({item, index}) =>
                 <TouchableOpacity
-                  style={styles.listItem}
-                  onPress={() => handleTaskToggle(index)}
-                  activeOpacity={0.7}
+                    style={styles.listItem}
+                    onPress={() => handleCheckboxPress(index)}
+                    activeOpacity={0.7}
                 >
-                  <Text style={item.completed ? styles.listItemTextCompleted : styles.listItemText}>
+                  <Text style={item.completed
+                      ? styles.listItemTextCompleted
+                      : styles.listItemText}>
                     {item.name}
                   </Text>
                   <View style={styles.checkBox}>
@@ -65,6 +83,14 @@ const ChoreList = () => {
             <Text style={styles.addButtonText}>Add</Text>
           </TouchableOpacity>
         </View>
+
+        <Modal visible={message !== null} animationType="slide">
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>{message}</Text>
+            <Button title="OK" onPress={() => setMessage(null)}/>
+          </View>
+        </Modal>
+
         {Platform.OS === 'ios'
             ? <KeyboardAvoidingView behavior="padding"/>
             : null}
@@ -74,30 +100,30 @@ const ChoreList = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   listItem: {
     height: 70,
     paddingHorizontal: 30,
     borderBottomWidth: 1,
-    borderBottomColor: "#AAA",
+    borderBottomColor: '#AAA',
     flex: 1,
     flexGrow: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between"
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   listItemText: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginLeft: 20,
-    textDecorationLine: "none"
+    textDecorationLine: 'none',
   },
   listItemTextCompleted: {
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginLeft: 20,
-    textDecorationLine: "line-through"
+    textDecorationLine: 'line-through',
   },
   checkBox: {
     marginRight: 20,
@@ -105,55 +131,64 @@ const styles = StyleSheet.create({
     width: 20,
     height: 20,
     borderWidth: 2,
-    borderColor: "#000",
-    alignItems: "center",
-    justifyContent: "center"
+    borderColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   checkBoxCompleted: {
     marginRight: 20,
     borderRadius: 5,
     width: 20,
     height: 20,
-    backgroundColor: "#000",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   listForm: {
     flexBasis: 275,
     flex: 0,
     margin: 15,
     padding: 15,
-    backgroundColor: "#CCC"
+    backgroundColor: '#CCC',
   },
   listName: {
     height: 50,
     padding: 15,
-    fontWeight: "600",
+    fontWeight: '600',
     marginRight: 50,
     marginBottom: 15,
-    borderColor: "#555",
-    borderWidth: 2
+    borderColor: '#555',
+    borderWidth: 2,
   },
   item: {
     height: 50,
     padding: 15,
     marginLeft: 50,
     marginBottom: 15,
-    borderColor: "#555",
-    borderWidth: 2
+    borderColor: '#555',
+    borderWidth: 2,
   },
   addButton: {
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     height: 50,
-    backgroundColor: "#000",
-    color: "#FFF"
+    backgroundColor: '#000',
+    color: '#FFF',
   },
   addButtonText: {
-    color: "#FFF",
-    fontWeight: "600",
-    fontSize: 20
-  }
+    color: '#FFF',
+    fontWeight: '600',
+    fontSize: 20,
+  },
+  modalContent: {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+},
+modalText: {
+  fontSize: 24,
+  marginBottom: 16,
+},
 });
 
 export default ChoreList;
---
+++ positiveMessages.js
@@ -0,0 +1,8 @@
+export const positiveMessages = [
+  'You are doing a great job!',
+  'Your hard work and dedication will pay off!',
+  'You are loved and appreciated!',
+  'Your positive attitude will lead to success!',
+  'Believe in yourself and your abilities!',
+  'You are making progress, keep going!',
+];
